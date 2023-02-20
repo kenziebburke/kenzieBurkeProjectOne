abec2b4075ada9c642c1d64330ef467f527c022d
Kenzie Burke <kenzie8829@gmail.com>
2023-02-20 14:28:05 -0500

    added JS modal

---
+++ script.js
@@ -0,0 +1,45 @@
+// declaring variables
+const modalButtonOpen = document.getElementById('openModal');
+
+const modalWindow = document.getElementById('modalBackground');
+
+const modalCloseButton = document.getElementById('closeModal');
+
+
+// open modal
+const openModal = function (){
+    modalWindow.style.display = 'flex';
+}
+modalButtonOpen.addEventListener('click', openModal);
+
+
+// exit modal
+const  closeModal = function () {
+    modalWindow.style.display = 'none';
+}
+modalCloseButton.addEventListener ('click', closeModal);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// pseudo code for img carosel
+
+
+
+
+
+
+// pseudo code for hambutget menu 
+
+
+
+
